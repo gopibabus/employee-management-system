30589477f1a2cabe35e65249f88f517817bfa7d7
gopibabu <s.gopibabu@gmail.com>
2020-12-06 17:45:40 -0600

    Apply fixes from StyleCI (#6)

--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -1,13 +1,11 @@
 <?php
 
-
 namespace App\Http\Controllers;
-
 
 class ReviewsController
 {
-    public function index(){
+    public function index()
+    {
         return view('reviews');
     }
-
 }
