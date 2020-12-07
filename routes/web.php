2ad7142904979b62b602b4ee69207d55ceb682fa
gopibabu <s.gopibabu@gmail.com>
2020-12-06 18:35:58 -0600

    Apply fixes from StyleCI (#8)

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,3 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/time-sheets', [App\Http\Controllers\TimesheetsController::class, 'index']);
-
