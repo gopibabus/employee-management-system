94274433164b2e2d3f2940878f223bd53806002e
Venkata siva naga mani kumar Garikipati <mani@Venkatas-MacBook-Pro.local>
2020-12-06 18:35:08 -0600

    feat(add) : new time-sheets route,controller,twig changes

---
+++ resources/views/timesheets.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container jumbotron">
+        <h1 class="display-4">Welcome to Timesheets</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/time-sheets', [App\Http\Controllers\TimesheetsController::class, 'index']);
+
